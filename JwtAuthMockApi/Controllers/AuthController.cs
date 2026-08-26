5cb1eed0ad102d06af55a07456ed29248d46e2ee
TheSwimon <lordkipanidzegeorge@gmail.com>
2026-08-26 15:17:55 +0400

    initial commit

---
+++ JwtAuthMockApi/Controllers/AuthController.cs
@@ -0,0 +1,52 @@
+﻿using JwtAuthMockApi.Entities;
+using JwtAuthMockApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace JwtAuthMockApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private static User _user = new User();
+
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto request)
+        {
+            string hashedPassword = new PasswordHasher<User>()
+                .HashPassword(_user, request.Password);
+
+            _user.Username = request.Username;
+            _user.PasswordHash = hashedPassword;
+
+            return Ok(_user);
+
+        }
+
+
+        [HttpPost("login")]
+        public IActionResult Login(UserDto request)
+        {
+            if (_user.Username != request.Username)
+            {
+                return Unauthorized();
+            }
+
+            var hasher = new PasswordHasher<User>();
+            var result = hasher.VerifyHashedPassword(_user, _user.PasswordHash, request.Password);
+
+            if (result == PasswordVerificationResult.Failed)
+            {
+                return Unauthorized("Incorrect credentials");
+            }
+
+            return Ok(_user);
+        }
+
+        
+    }
+}
---
+++ JwtAuthMockApi/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿namespace JwtAuthMockApi.Entities
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+
+        public string PasswordHash { get; set; } = string.Empty;
+    }
+}
